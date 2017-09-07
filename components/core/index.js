dd836e4f8e65cd135c8f8590b4e2c58b0ee93f0a
blackstone86 <asianking86@qq.com>
2017-09-07 22:57:23 +0800

    [core/index.js] 组件核心增加bootstrap引入

--- components/core/index.js
+++ components/core/index.js
@@ -2,3 +2,5 @@
 
 window.$ = window.jQuery = require("../../component_modules/jquery/dist/jquery.min");
 require("../../component_modules/jquery-ui/jquery-ui.min");
+require("../../component_modules/bootstrap/dist/css/bootstrap.min.css");
+require("../../component_modules/bootstrap/dist/js/bootstrap.min");
