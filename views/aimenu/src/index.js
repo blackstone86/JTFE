fb412079e03f2ffe7fe0ccacffe33d7be4e85bc6
hjw <asianking86@qq.com>
2017-09-11 00:00:35 +0800

    [index.ejs] 增加实现菜单切换

--- views/aimenu/src/index.js
+++ views/aimenu/src/index.js
@@ -1,10 +1,31 @@
-require("../../../component_modules/jQuery-menu-aim/example/css/bootstrap-responsive.css");
 require("./style.less");
-require("../../../component_modules/jQuery-menu-aim/jquery.menu-aim");
 
 var sidebar = $('#sidebar');
+var navs = $('.sidebar-nav');
+var firstnav = navs.eq(0);
 var togglebtn = $('#sidebar-toggle');
+
 togglebtn.on('click', function(){
     $(this).toggleClass("icon-unfold");
     sidebar.toggleClass("sidebar-mini");
 });
+
+navs.on('click', function(){
+    var ele = $(this);
+    var istoggle = ele.hasClass('sidebar-nav-active');
+    var list = ele.find('ul');
+    var items = list.find('li');
+    
+    if(istoggle){
+        ele.removeClass('sidebar-nav-active');
+        list.css('maxHeight', 0);
+    }else{
+        navs.removeClass('sidebar-nav-active');
+        navs.find('ul').css('maxHeight', 0);
+    
+        ele.addClass('sidebar-nav-active');
+        list.css('maxHeight', items.length*40);
+    }
+});
+
+firstnav.click();
