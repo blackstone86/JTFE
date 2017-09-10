45ce938d7e7df977c9b09ed51f6573dfa6614838
hjw <asianking86@qq.com>
2017-09-10 13:30:50 +0800

    [aimenu] 初始化aimenu

---
+++ views/aimenu/src/aimenu.js
@@ -0,0 +1,3 @@
+require("../../../component_modules/jQuery-menu-aim/example/css/bootstrap-responsive.css");
+require("./style.less");
+require("../../../component_modules/jQuery-menu-aim/jquery.menu-aim");
