a93639f3d280d2690320182d6a3e88414e4b0476
hjw <asianking86@qq.com>
2017-09-10 13:29:58 +0800

    [index.js] 增加common.less引入

--- components/core/index.js
+++ components/core/index.js
@@ -1,6 +1,10 @@
 "use strict";
 
+// 样式
+require("../common/common.less");
+require("../../component_modules/bootstrap/dist/css/bootstrap.css");
+
+// 脚本
 window.$ = window.jQuery = require("../../component_modules/jquery/dist/jquery.min");
 require("../../component_modules/jquery-ui/jquery-ui.min");
-require("../../component_modules/bootstrap/dist/css/bootstrap.css");
 require("../../component_modules/bootstrap/dist/js/bootstrap.min");
