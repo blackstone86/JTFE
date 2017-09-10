5e4c18012b93493d93b6ffedd03f3abfb352b27b
hjw <asianking86@qq.com>
2017-09-10 23:24:30 +0800

    [aimenu] 实现菜单选择

--- views/aimenu/src/index.js
+++ views/aimenu/src/index.js
@@ -1,3 +1,10 @@
 require("../../../component_modules/jQuery-menu-aim/example/css/bootstrap-responsive.css");
 require("./style.less");
 require("../../../component_modules/jQuery-menu-aim/jquery.menu-aim");
+
+var sidebar = $('#sidebar');
+var togglebtn = $('#sidebar-toggle');
+togglebtn.on('click', function(){
+    $(this).toggleClass("icon-unfold");
+    sidebar.toggleClass("sidebar-mini");
+});
