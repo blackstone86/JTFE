8765cbca63a8134e578ce471734681bd4e642af9
hjw <asianking86@qq.com>
2017-09-10 16:55:36 +0800

    [gulp-config.js] 增加迁移甘特图自带图片字体

--- gulp_config.js
+++ gulp_config.js
@@ -32,14 +32,28 @@ exports.nocompressDirs = nocompressDirs;
 // 迁移资源内容（文件夹、文件）
 let transfesrc = 
 [
+    // 项目公共字体图标
     "./components/common/**",
     "!./components/common/**/*.less",
     "!./components/common/**/*.sass",
     "!./components/common/**/*.js",
+    // 甘特图自带字体图标
+    "./components/gantt/**",
+    "!./components/gantt/css",
+    "!./components/gantt/css/**",
+    "!./components/gantt/i18n",
+    "!./components/gantt/i18n/**",
+    "!./components/gantt/imgs",
+    "!./components/gantt/imgs/**",
+    "!./components/gantt/tpls",
+    "!./components/gantt/tpls/**",
+    "!./components/gantt/**/*.json",
+    "!./components/gantt/**/*.js",
 ];
 exports.transfesrc = transfesrc;
 
 // 当前浏览视图
 // let currview = "aimenu";
-let currview = "icon";
+// let currview = "icon";
+let currview = "gantt";
 exports.currview = currview;
